ed5c2d5b82a4d8e2159cddc9a837ac7a4c39b0f3
Isaac <Isaacsunoo@gmail.com>
2018-06-26 15:36:03 -0600

    complete objective 6 lab 4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,12 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int num = 0;
+    int sum = 0;
+
+    while(num <=20){
+      sum += num;
+      num++;
+    }
+    System.out.println(sum);
+  }
+}
