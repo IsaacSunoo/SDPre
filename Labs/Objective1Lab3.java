6cb5bcce7f88d1f851378ca789a1ea903fd5853f
Isaac <Isaacsunoo@gmail.com>
2018-06-26 00:22:38 -0600

    complete objective 1 lab 3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main(String[] args) {
+    System.out.println ("Name : Isaac Sunoo");
+    System.out.println ("Age : 23");
+    System.out.println ("State : Colorado");
+  }
+}
