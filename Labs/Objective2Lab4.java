74502371677564a7a35c11b5f1ead06b3ab87ae5
Isaac <Isaacsunoo@gmail.com>
2018-06-26 00:47:08 -0600

    complete objective 2 lab 4

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,12 @@
+public class Objective2Lab4 {
+  public static void main (String [] args) {
+    double num1, num2, num3, average;
+
+    num1 = 88;
+    num2 = 12;
+    num3 = 21;
+    average = (num1 + num2 + num3)/3;
+
+    System.out.println("The average of 88, 12, and 21 is " + average);
+  }
+}
