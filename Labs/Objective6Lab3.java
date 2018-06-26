eee4c8f6f140ccc4264a3ba062f8289106eaa3d1
Isaac <Isaacsunoo@gmail.com>
2018-06-26 15:20:49 -0600

    complete objective 6 lab 3

---
+++ Labs/Objective6Lab3.java
@@ -0,0 +1,15 @@
+public class Objective6Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    while(counter <=20) {
+      System.out.print (counter );
+      counter = counter + 1;
+    if(counter % 2 ==0)
+      System.out.print(" is even");
+    else
+      System.out.print(" is odd");
+      System.out.println();
+    }
+  }
+}
