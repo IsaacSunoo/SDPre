8c970a36e91ae51da05e44ab5561bc1ee565aa3f
Isaac <Isaacsunoo@gmail.com>
2018-06-25 18:24:27 -0600

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ){
+		System.out.println( "I, Isaac, am determined to learn how to code." );
+		System.out.println( "Today's date is May 29, 2018" );
+	}
+}
